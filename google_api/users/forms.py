0c6c8ee1db8b9ab6bbc3c79223ff95f6d3a0726d
Alexey <suxovarov.alexey@gmail.com>
2023-01-20 11:02:42 +0300

    UserProfileForm

--- google_api/users/forms.py
+++ google_api/users/forms.py
@@ -20,3 +20,20 @@ class UserForm(UserCreationForm):
                                                                   'class': 'password'}))
     # reCAPTCHA token
     token = forms.CharField(widget=forms.HiddenInput())
+
+
+class UserProfileForm(forms.ModelForm):
+    """
+    Basic model-form for our user profile that extends Django user model.
+    """
+    address = forms.CharField(max_length=100, required=True, widget=forms.HiddenInput())
+    town = forms.CharField(max_length=100, required=True, widget=forms.HiddenInput())
+    county = forms.CharField(max_length=100, required=True, widget=forms.HiddenInput())
+    post_code = forms.CharField(max_length=8, required=True, widget=forms.HiddenInput())
+    country = forms.CharField(max_length=40, required=True, widget=forms.HiddenInput())
+    longitude = forms.CharField(max_length=50, required=True, widget=forms.HiddenInput())
+    latitude = forms.CharField(max_length=50, required=True, widget=forms.HiddenInput())
+
+    class Meta:
+        model = UserProfile
+        fields = ('address', 'town', 'county', 'post_code', 'country', 'longitude', 'latitude')
